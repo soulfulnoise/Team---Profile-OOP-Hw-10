8af394346d6ffbc50c25671ec37b302fab5b431d
The Noise <leespaulding25@gmail.com>
2021-03-06 02:04:45 -0500

    created intern.test.js

--- _test_/Enigineer.test.js
+++ _test_/Enigineer.test.js
@@ -1,2 +1,18 @@
-const { resourceUsage } = require("process");
+const { it } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+describe("grab employees github account from the constructor" ,() => {
+    describe("showGitHub", () => {
+        it("should return employees Github Url", () => {
+            const devpro = new Engineer();
+            const team =new Employee("FOO");
+
+            const url = devpro.buildUrl(team);
+
+            expect(url).toEqual("Engineer");
+        })
+    })
+   
+});
 
---
+++ _test_/Intern.test.js
@@ -0,0 +1,18 @@
+const { describe } = require("yargs");
+const Intern = require("./li\Intern.js");
+
+const testValue = "UGA";
+const student = new Intern("Foo" , 1, 'test@testcom' , testValue);
+expect(student.school).toBe(testValue);
+
+test('getRole() should return "Intern"', () => {
+const testValue = 'Intern';
+const student = new Intern('Foo', 1, 'test@test.com', 'UGA');
+expect(student.getRole()).toBe(testValue);
+});
+
+test('Can get school via getSchool()', () => {
+const testValue = 'UGA';
+const student = new Intern('Foo', 1, 'test@test.com', testValue);
+expect(student.getSchool()).toBe(testValue);
+});
